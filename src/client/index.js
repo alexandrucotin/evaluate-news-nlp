6442fe946b895b4245095fe60f6f922d5edde328
Alexandru Cotin <a.cotin96@gmail.com>
2020-05-06 11:57:41 +0200

    first commit

---
+++ src/client/index.js
@@ -0,0 +1,7 @@
+//JS
+import { alert } from "./js/alert";
+
+//SCSS
+import "./style/main.scss";
+
+export { alert };
---
+++ webpack.prod.js
@@ -0,0 +1,38 @@
+const path = require("path");
+const webpack = require("webpack");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  entry: "./src/client/index.js",
+  mode: "production",
+  module: {
+    rules: [
+      {
+        test: "/.js$/",
+        exclude: /node_modules/,
+        loader: "babel-loader",
+      },
+      {
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./src/client/views/index.html",
+      filename: "./index.html",
+    }),
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+    }),
+  ],
+  resolve: {
+    modules: [path.resolve(__dirname, "src/client/styles"), "node_modules"],
+  },
+  output: {
+    library: "client",
+    libraryTarget: "var",
+  },
+};
